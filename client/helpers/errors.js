1dc3ca10a08bc6c5ebfdb958a4927a459885083b
germify <germify@users.noreply.github.com>
2015-03-31 22:51:43 -0500

    Clear errors after 3 seconds.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -4,3 +4,10 @@ Errors = new Mongo.Collection(null);
 throwError = function(message) {
     Errors.insert({message: message});
 };
+
+Template.error.rendered = function() {
+    var error = this.data;
+    Meteor.setTimeout(function () {
+        Errors.remove(error._id);
+    }, 3000);
+};
