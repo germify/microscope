6784068affd4cc10ecddcda511de02af2f1d8300
germify <germify@users.noreply.github.com>
2015-04-04 21:52:01 -0500

    Sending custom user data.

---
+++ client/main.js
@@ -0,0 +1,12 @@
+Tracker.autorun(function(){
+    if (Meteor.user() && !Meteor.loggingIn()) {
+        var intercomSettings = {
+            name: Meteor.user().username,
+            email: Meteor.user().emails[0].address,
+            created_at: Math.round(Meteor.user().createdAt/1000),
+            favorite_color: _.sample(['blue','red','green','yellow']),
+            app_id: "jr2vys17"
+        };
+        Intercom('boot', intercomSettings);
+    }
+});
