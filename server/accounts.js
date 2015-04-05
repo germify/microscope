2a6ca69d620382ee7ce848cdb33f28498b01bfce
germify <germify@users.noreply.github.com>
2015-04-05 16:09:08 -0500

    Delete intercom

--- server/accounts.js
+++
@@ -1,8 +0,0 @@
-Accounts.onCreateUser(function(options, user) {
-    user.intercomHash = IntercomHash(user, '08f626366f2c8f92cb1beb1653d71adbc1d4b33a');
-
-    if (options.profile)
-        user.profile = options.profile;
-
-    return user;
-});
--- server/migrations.js
+++ server/migrations.js
@@ -1,24 +1,6 @@
 Migrations.add({
-    name: 'Add intercom hash to users.',
-    version: 1,
-
-    up: function() {
-        var users = Meteor.users.find({intercomHash: {$exists: false}});
-        users.forEach(function(user) {
-            Meteor.users.update(user._id, {$set: {
-                intercomHash: IntercomHash(user, '08f626366f2c8f92cb1beb1653d71adbc1d4b33a')
-            }});
-        });
-    },
-
-    down: function() {
-        Meteor.users.update({}, {$unset: {intercomHash: true}}, {multi: true});
-    }
-});
-
-Migrations.add({
     name: 'Add bitly shorturl to posts.',
-    version: 2,
+    version: 1,
 
     up: function() {
         var posts = Posts.find({shortUrl: {$exists: false}});
@@ -35,8 +17,7 @@ Migrations.add({
 });
 
 // migrate up
-//Migrations.migrateTo(1);
-Migrations.migrateTo(2);
+//Migrations.migrateTo('latest');
 
 // migrate down
 //Migrations.migrateTo(0);
