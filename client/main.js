9839e8da101acc332d5932888959c0394b7c5ccf
germify <germify@users.noreply.github.com>
2015-04-04 22:16:03 -0500

    Enabling secure mode.

--- client/main.js
+++ client/main.js
@@ -5,6 +5,8 @@ Tracker.autorun(function(){
             email: Meteor.user().emails[0].address,
             created_at: Math.round(Meteor.user().createdAt/1000),
             favorite_color: _.sample(['blue','red','green','yellow']),
+            user_id: Meteor.user()._id,
+            user_hash: Meteor.user().intercomHash,
             app_id: "jr2vys17"
         };
         Intercom('boot', intercomSettings);
