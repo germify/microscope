059059c3180ea7a575846d594d8112005ad6b370
germify <germify@users.noreply.github.com>
2015-04-04 20:24:58 -0500

    Distributing Bitly API calls.

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -39,10 +39,27 @@ Bitly.getClicks = function(link){
     );
     if(statsResponse.data.status_code === 200)
         return statsResponse.data.data.link_clicks
-}
+};
 
 Meteor.methods({
     'getBitlyClicks': function(link){
         return Bitly.getClicks(link);
     }
 });
+
+var callInterval = 10000; // 1000ms * 10 = 10s
+Meteor.setInterval(function(){
+    // get all posts with the shortUrl property
+    var shortUrlPosts = Posts.find({shortUrl: { $exists: true }});
+    var postsNumber = shortUrlPosts.count();
+    // initialize counter
+    var count = 0;
+    shortUrlPosts.forEach(function(post){
+        // calculate the right delay to distribute API calls evenly throughout the interval
+        var callTimeout = Math.round(callInterval/postsNumber*count);
+        Meteor.setTimeout(function(){
+            Posts.update(post._id, {$set: {clicks: Bitly.getClicks(post.shortUrl)}});
+        }, callTimeout);
+        count++;
+    });
+}, callInterval);
